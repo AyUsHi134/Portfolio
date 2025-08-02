1d631a2fb132b2e9661a5919b0884137262f5527
Ayushi Singh <singhayushi9369@gmail.com>
2025-08-03 01:26:46 +0530

    About Routes added to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import aboutRoutes from "./routes/aboutRoutes.js";
 
 dotenv.config();
 connectDB();  
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/about", aboutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT ${process.env.PORT}`)
