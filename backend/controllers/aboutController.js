fd219d12a566f47ad13c491bb225a6499c4271fe
Ayushi Singh <singhayushi9369@gmail.com>
2025-08-02 23:16:01 +0530

    AboutController

---
+++ backend/controllers/aboutController.js
@@ -0,0 +1,30 @@
+import About from "../models/aboutModel.js";
+
+const getAbout = async (req, res) => {
+    try{
+        const aboutData = await About.find();
+        res.json(aboutData[0]);
+    } catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+}
+
+const updateAbout = async (req,res) => {
+    try {
+        const existingAbout = await About.findOne();
+        if (existingAbout){
+            existingAbout.set(req.body);
+            await existingAbout.save();
+            res.json(existingAbout);
+        } else {
+            const newAbout = new About(req.body);
+            await newAbout.save();
+            res.json(newAbout);
+        }
+    } catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+export {getAbout, updateAbout};
